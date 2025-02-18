5fc8eb9d1eb5ee957a21f5285442a13631176967
sriranjini0605 <sriranjinirvasista@gmail.com>
2025-02-18 15:35:12 -0700

    fixed minor bugs

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,30 +1,29 @@
 import { Link } from "react-router-dom";
 import { Carousel, Image } from "react-bootstrap";
-import Loader from "./Loader";
 import Message from "./Message";
 import { useGetTopProductsQuery } from "../slices/productApiSlice";
 
-
 const ProductCarousel = () => {
-
   const { data: products, isLoading, error } = useGetTopProductsQuery();
-  console.log("top products", products);
 
-  return isLoading ? <Loader/> : error ? <Message variant = 'danger'>{error}</Message> : (
-    <Carousel pause = 'hover' className = 'bg-primary mb-4'>
-        {products.map(product => (
-            <Carousel.Item key = {product._id}>
-              <Link to = {`/product/${product._id}`}>
-                <Image src = {product.image} alt = {product.name} fluid />
-                <Carousel.Caption>
-                  <h3>{product.name}</h3>
-                  <p>{product.description.substring(0, 100)}...</p>
-                </Carousel.Caption>
-              </Link>
-            </Carousel.Item>
-        ))}
+  return isLoading ? null : error ? (
+    <Message variant="danger">{error?.data?.message || error.error}</Message>
+  ) : (
+    <Carousel pause="hover" className="bg-primary mb-4">
+      {products.map((product) => (
+        <Carousel.Item key={product._id}>
+          <Link to={`/product/${product._id}`}>
+            <Image src={product.image} alt={product.name} fluid />
+            <Carousel.Caption className="carousel-caption">
+              <h2 className="text-white text-right">
+                {product.name} (${product.price})
+              </h2>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
+      ))}
     </Carousel>
-  )
-}
+  );
+};
 
-export default ProductCarousel
+export default ProductCarousel;
